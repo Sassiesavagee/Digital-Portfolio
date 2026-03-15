e3ff51d76f85d8ce126224fef7cca3e83d5659f4
Sassiesavagee <Sharde232@gmail.com>
2026-03-14 20:29:12 -0500

    adding CPR certifications. Made an additional json page for the CPR-card, then I linked it to the Credentials.html page. Also, updated the wording in all languages for the CPR-card.

--- Credentials2.js
+++ Credentials2.js
@@ -1,24 +1,15 @@
 // ...existing code...
 document.addEventListener('DOMContentLoaded', () => {
   const images = [
-    "IS-1000_screenshot.png",
-    "IS-1001_screenshot.png",
-    "IS-1002_screenshot.png",
-    "IS-1006_screenshot.png",
-    "IS-1007_screenshot.png",
-    "IS-1008_screenshot.png",
-    "IS-1009_screenshot.png",
-    "IS-1011_screenshot.png",
-    "IS-1012_screenshot.png",
-    "IS-1013_screenshot.png",
-    "IS-1015_screenshot.png",
-    "IS-1016_screenshot.png"
+    "Adult First Aid Certification.jpeg",
+    "Basic Life Support Certification.jpeg",
+    "Emergency Use of Medical Oxygen Certification.jpeg",
   ];
   let currentIndex = 0;
 
-  const imgEl = document.getElementById('FEMA-img');
-  const prevBtn = document.getElementById('prev-btn');
-  const nextBtn = document.getElementById('next-btn');
+  const imgEl = document.getElementById('CPR-img');
+  const prevBtn = document.getElementById('CPR-prev-btn');
+  const nextBtn = document.getElementById('CPR-next-btn');
 
   function updateImage() {
     if (imgEl) imgEl.src = images[currentIndex];
